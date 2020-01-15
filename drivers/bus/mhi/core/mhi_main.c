ac9d9857fb72a690e698c384f41e4816e5073712
Hemant Kumar <hemantk@codeaurora.org>
2020-01-14 17:35:10 -0800

    mhi: core: Add range check for channel id received in event ring
    The mhi_process_cmd_completion function reads cmd channel id from
    cmd_pkt using MHI_TRE_GET_CHID, the value is under the control of MHI
    devices and can be any value between 0 and 255. However the max channel
    is defined in device tree file and it is usually smaller than 255. This
    can cause out of bound access to the channel array. Fix this by checking
    the channel id received in cmd ring against the max channel allowed on
    target.
    
    Change-Id: Ib6faf67c7eae67186b3a44e6b1612deff6bf05fa
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -1144,7 +1144,7 @@ static void mhi_process_cmd_completion(struct mhi_controller *mhi_cntrl,
 		chan = MHI_TRE_GET_CMD_CHID(cmd_pkt);
 		if (chan >= mhi_cntrl->max_chan) {
 			MHI_ERR("invalid channel id %u\n", chan);
-			break;
+			goto del_ring_el;
 		}
 		mhi_chan = &mhi_cntrl->mhi_chan[chan];
 		write_lock_bh(&mhi_chan->lock);
@@ -1154,6 +1154,7 @@ static void mhi_process_cmd_completion(struct mhi_controller *mhi_cntrl,
 		break;
 	}
 
+del_ring_el:
 	mhi_del_ring_element(mhi_cntrl, mhi_ring);
 }
 
